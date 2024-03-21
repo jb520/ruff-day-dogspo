71b8aaa7e4976104930d49e0a350dca175f70a1a
Jennifer Rahman <81711306+jb520@users.noreply.github.com>
2024-03-21 22:00:24 +0000

    create quote array

--- app.rb
+++ app.rb
@@ -37,7 +37,26 @@ post("/ruff_day") do
 
     @dog_name = "A picture of the #{dog_name_found} breed of dog."
   end
-   
+  
+  dog_inspirational_quotes = [
+  "Paws and reflect on the good in life.",
+  "Keep barking up the right tree, success is just a bark away.",
+  "Every dog has its day, so make today yours.",
+  "Life is ruff, but you've got the bark to beat it.",
+  "Chase your dreams like you chase your tail – with endless joy.",
+  "Unleash your potential, the world is your dog park.",
+  "Be the person your dog thinks you are – kind, brave, and unstoppable.",
+  "When life gets tough, just paws and enjoy the simple things.",
+  "Stay pawsitive, even when life throws you a bone.",
+  "Keep fetching your dreams, persistence is key.",
+  "Dig deep for the courage to overcome obstacles.",
+  "Sniff out opportunity and run towards it with all your might.",
+  "Let sleeping dogs lie, but when you're awake, chase your dreams.",
+  "In the journey of life, enjoy the walk, not just the destination.",
+  "Sometimes the best approach is to lie down and roll with it."
+  ]
+
+  @dog_inspo = dog_inspirational_quotes.sample
 
   erb(:process_form)
 end
