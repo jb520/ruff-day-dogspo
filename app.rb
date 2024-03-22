3e3ebb58ef26dbe92075918efd311bbed64a0d0f
Jennifer Rahman <81711306+jb520@users.noreply.github.com>
2024-03-22 03:45:11 +0000

    refactor code

--- app.rb
+++ app.rb
@@ -1,19 +1,7 @@
 require "sinatra"
 require "sinatra/reloader"
-require "http"
 
 get("/") do
-=begin
-  dog_key = ENV.fetch("DOGAPI_KEY")
-  dog_url = "https://api.thedogapi.com/v1/images/search?api_key=#{dog_key}"
-  dog_data = HTTP.get(dog_url).to_s
-  dog_parse = JSON.parse(dog_data)
-  results_arr = dog_parse.at(0)
-  @img_url = results_arr.fetch("url")
-  breed_info = results_arr.fetch("breeds")
-  breed_hash = breed_info.at(0)
-  @dog_name = breed_hash.fetch("name") 
-=end
 
   erb(:home)
 end
